3312eba0b0527483d68b180797a82b586662f60f
GedikEngin <74453024+GedikEngin@users.noreply.github.com>
2021-04-23 13:06:53 +0100

    chore: annotate and reformat code

--- gui/description.py
+++ gui/description.py
@@ -1,31 +1,28 @@
 from tkinter import *
 
-class Description(LabelFrame):
+class Description(LabelFrame): # inherit directly from tkinter
 
     def __init__(self, parent, text=''):
 
-        super(Description, self).__init__(parent, text=text) #possibly mention inheritence in design -- gui parent
+        super(Description, self).__init__(parent, text=text)   # possibly mention inheritance in design -- gui parent
 
-        self.sv_item_desc = StringVar()
-        self.text_item_desc = Text(self, width=50, height=58, bg="grey50")
-        self.text_item_desc.tag_configure("bold", font="Helvetica 12 bold")
-        self.text_item_desc.tag_configure("normal", font="Helvetica 12")
+        self.sv_item_desc = StringVar() # creation and config of text box
+        self.text_item_desc = Text(self, width=50, height=58, bg="grey50")  # box config
+        self.text_item_desc.tag_configure("bold", font="Helvetica 12 bold")  # title config
+        self.text_item_desc.tag_configure("normal", font="Helvetica 12")    # main body config
+        self.text_item_desc.config(state=DISABLED) # disables text box to prevent further editing
+        self.text_item_desc.grid(row=1, column=0, padx=0, pady=0)   # alignment
 
-        self.text_item_desc.config(state=DISABLED)
-        self.text_item_desc.grid(row=1, column=0, padx=0, pady=0)
-
-    def update_text(self, desc, title='', append=False):
-        self.text_item_desc.config(state=NORMAL)
+    def update_text(self, desc, title='', append=False):   # text updating function
+        self.text_item_desc.config(state=NORMAL)    # allows textbox to be edited
         if not append:
-            self.text_item_desc.delete('1.0', END)
+            self.text_item_desc.delete('1.0', END)  # deletes everything from first character to the end of the textbox
 
-        self.text_item_desc.insert(END, title+'\n', "bold")
+        self.text_item_desc.insert(END, title+'\n', "bold")  # formatting title line
+        self.text_item_desc.insert(END, desc + '\n\n\n', "normal")  # formatted main text
+        self.text_item_desc.config(state=DISABLED)  # shuts textbox back
 
-        self.text_item_desc.insert(END, desc + '\n\n\n', "normal")
-
-        self.text_item_desc.config(state=DISABLED)
-
-if __name__ == '__main__':
+if __name__ == '__main__':#
     window = Tk()
     descv = Description(window)
     descv.pack()

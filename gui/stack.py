d1c42aedefa2b24cca504b0b9e8dc16ed262f8fa
GedikEngin <74453024+GedikEngin@users.noreply.github.com>
2021-05-02 12:14:24 +0100

    chore: annotate and reformat code

--- gui/stack.py
+++ gui/stack.py
@@ -7,24 +7,24 @@ class StackClass():
 
     # accessors
 
-    def get_stack_elements(self):
+    def get_stack_elements(self):       # accessor method to get stack
         return self.stack
 
-    def get_stack_top_element(self):
+    def get_stack_top_element(self):        # accessor method dedicated to top element of stack
         if not self.stack:
             return None
 
         return self.stack[-1]
 
-    def get_stack_length(self):
+    def get_stack_length(self):     # accessor method for length of stack
         return len(self.stack)
 
     # mutators
 
-    def push_stack(self, item):
+    def push_stack(self, item):     # mutator method for pushing into stack
         self.stack.append(item)
 
-    def pop_stack(self):
+    def pop_stack(self):        # mutator method for popping from stack
         if not self.stack:
             return None
         top_item = self.get_stack_top_element()
